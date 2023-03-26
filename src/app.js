e9212e2a96239d2899c2963ac7310ba37c0dc143
CabraCrimes <benalexanderthegreat@gmail.com>
2023-03-26 10:32:11 +0000

    Dimaon Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let exten = ["com ", "net ", "us ", "io "];
+
+  const domainLoop = (arrOfPronoun, arrOfAdj, arrOfNoun, arrOfExten) => {
+    let arrOfDomain = [];
+    for (let i = 0; i < arrOfAdj.length; i++) {
+      for (let j = 0; j < arrOfPronoun.length; j++) {
+        for (let k = 0; k < arrOfNoun.length; k++) {
+          for (let l = 0; l < arrOfExten.length; l++) {
+            arrOfDomain.push(
+              `${arrOfPronoun[j]}${arrOfAdj[i]}${arrOfNoun[k]}.${arrOfExten[l]}`
+            );
+          }
+        }
+      }
+    }
+    return arrOfDomain;
+  };
+  let domainGenerator = domainLoop(pronoun, adj, noun, exten);
+  console.log(domainGenerator);
+
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#dom").innerHTML = domainGenerator;
+  });
 };
